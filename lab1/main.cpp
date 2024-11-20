03788840b16f42258a67b11fb6fe5b6d87310aff
Анастасия Мытарева <nastya.mytareva.04@gmail.com>
2024-11-20 23:25:13 +0400

    Bubble sorting in C

---
+++ lab1/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#define MAX_SIZE 100
+
+int arr[MAX_SIZE];
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    printf("Enter the array elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
